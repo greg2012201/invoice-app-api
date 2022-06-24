701e83d2ddfed719e16526299a5fc72e90cc0c53
Grzegorz Dubiel <greg2012201@gmail.com>
2022-06-25 00:24:31 +0200

    Add linkSchema.

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,3 +1,14 @@
-import Contractor from './Contractor'
+import { gql } from 'apollo-server-express'
+import contractor from './Contractor.js'
 
-export default [Contractor]
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+
+  type Mutation {
+    _: Boolean
+  }
+
+`
+export default [linkSchema, contractor]
