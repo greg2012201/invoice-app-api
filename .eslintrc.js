efb14109adc8f457cd4d55a86a76d355b2066648
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-02 00:12:09 +0200

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   extends: ['eslint:recommended', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     sourceType: 'module',
     allowImportExportEverywhere: false,
     codeFrame: true,
   },
+  plugins: ['prettier'],
   env: {
     es2021: true,
     node: true,
+  },
+  babelOptions: {
+    configFile: './.babelrc',
   },
   rules: {
     'no-console': 'off',
