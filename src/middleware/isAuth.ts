23178cbaea5a0b7cfedb6f029491f56dbd0224b1
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-31 23:41:11 +0200

    Use higher order guard resolver pattern for isAuth fn.

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,27 +1,37 @@
 import { Request, Response } from 'express';
 import { verify, JwtPayload } from 'jsonwebtoken';
+import { IMe } from 'types';
+
+interface Payload extends JwtPayload {
+  userId: string;
+}
 
 interface Context {
   req: Request;
   response: Response;
-  payload: JwtPayload;
+  payload: Payload;
+  me: IMe;
+  models: any;
 }
+export const isAuth =
+  (next: (parent: any, args: any, context: Context, info: any) => void) =>
+  async (parent: any, args: any, context: Context, info: any) => {
+    const authorization = context.req.headers['authorization'];
 
-export const isAuth = ({ context }: { context: Context }, next: () => void) => {
-  const authorization = context.req.headers['authorization'];
+    if (!authorization) {
+      throw new Error('not authenticated');
+    }
 
-  if (!authorization) {
-    throw new Error('not authenticated');
-  }
+    try {
+      const token = authorization.split(' ')[1];
+      const payload: any = verify(token, process.env.ACCESS_TOKEN_SECRET!);
 
-  try {
-    const token = authorization.split(' ')[1];
-    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
-    context.payload = payload as any;
-  } catch (err) {
-    console.log(err);
-    throw new Error('not authenticated');
-  }
+      context.payload = payload.userId;
+      context.me = { id: payload.userId };
+    } catch (err) {
+      console.log(err);
+      throw new Error('not authenticated');
+    }
 
-  return next();
-};
+    return next(parent, args, context, info);
+  };
