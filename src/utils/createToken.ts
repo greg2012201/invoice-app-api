91d8c10c60bb0da9923027ca3b48878a048cd11d
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-31 23:40:04 +0200

    Fix id key.

--- src/utils/createToken.ts
+++ src/utils/createToken.ts
@@ -2,13 +2,21 @@ import { Jwt, sign } from 'jsonwebtoken';
 import { IUser } from 'types';
 
 export const createAccessToken = (user: IUser): string => {
-  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: '15m',
-  });
+  return sign(
+    { userId: user._id.toString() },
+    process.env.ACCESS_TOKEN_SECRET!,
+    {
+      expiresIn: '15m',
+    }
+  );
 };
 
 export const createRefreshToken = (user: IUser): string => {
-  return sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET!, {
-    expiresIn: '7d',
-  });
+  return sign(
+    { userId: user._id.toString() },
+    process.env.REFRESH_TOKEN_SECRET!,
+    {
+      expiresIn: '7d',
+    }
+  );
 };
