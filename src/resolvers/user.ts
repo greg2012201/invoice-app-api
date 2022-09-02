74b6b2aa71f510b6d6390690468b4f75c4000750
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-02 23:42:25 +0200

    Fix error message and create register mutation.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,6 @@
 import { isAuth } from 'middleware/isAuth';
 import mongoose from 'mongoose';
+import { hash } from 'bcryptjs';
 import { IUser, IMe } from 'types';
 
 const user = {
@@ -18,11 +19,32 @@ const user = {
           }
           return user;
         } catch (e) {
-          console.log(`Error happened at Query getContractors ${args}`);
+          console.log(`Error happened at Query getMe ${args}`);
           return e;
         }
       }
     ),
+  },
+  Mutation: {
+    register: async (
+      parent: any,
+      args: { password: string; email: string },
+      { models: { User }, me }: { models: { User: any }; me: IMe },
+      info: any
+    ): Promise<boolean | any> => {
+      try {
+        const hashedPassword = await hash(args.password, 12);
+        const newUser: IUser = await new User({
+          _id: new mongoose.Types.ObjectId().toString(),
+          email: args.email,
+          password: hashedPassword,
+        }).save();
+        return true;
+      } catch (e) {
+        console.log(`Error happened at Query register ${args}`);
+        return e;
+      }
+    },
   },
 };
 export default user;
